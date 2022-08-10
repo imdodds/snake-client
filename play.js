4512499833fab17f717195b8ba4f78bfea9b993f
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-10 19:49:26 +0000

    set up connect function for snake game

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function (data) {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  console.log(data);
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();

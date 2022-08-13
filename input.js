9b9093a24f22448cd93a517c50506a765424613b
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-13 16:05:14 +0000

    implemented movement commands for wasd keys

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +17,16 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   };
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = setupInput;
